8273f916668140bed5f89601c471b6aefa1209b5
Eliezer Nardi <eliezerrn@gmail.com>
2023-03-01 00:47:40 -0300

    Teste commit itellij

---
+++ Teste2/src/Main.java
@@ -0,0 +1,12 @@
+import Enum.Nivel;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        System.out.println(Nivel.PLENO.getDescricao());
+
+
+
+    }
+}
